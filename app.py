28b16b35b8dc4e7abf09e9735a59da7fdc03c1b2
sumeet1512 <109270155+sumeet1512@users.noreply.github.com>
2024-11-12 12:00:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns 
 
-st.title("Sales KPI Dashboard with Streamlit")
+st.title("Sales Dashboard with Streamlit")
 
 st.markdown('## Key Metrics')
 
